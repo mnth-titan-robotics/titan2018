2a9a6e797178afd22b4ca0310815f43b09f0820a
Kevin Chen <kevin.chen@kuka.com>
2018-02-01 10:08:50 -0600

    assignment for 02/01

--- src/org/usfirst/frc/team2789/robot/Robot.java
+++ src/org/usfirst/frc/team2789/robot/Robot.java
@@ -1,9 +1,43 @@
 package org.usfirst.frc.team2789.robot;
 
+/**
+ * TODO 01: IMPORT THE CLIMBER
+ * I went ahead and did this for you. Delete this comment when you're done
+ */
+import org.usfirst.frc.team2789.robot.subsystems.Climber;
 import org.usfirst.frc.team2789.robot.subsystems.DriveSystem;
 import org.usfirst.frc.team2789.robot.subsystems.OperatorInterface;
 
+/**
+ * TODO 02: IMPORT THE COMPRESSOR
+ * We also need to create and start a compressor object, so let's import it.
+ * I've gone ahead and done it for you. Delete this comment when you're done
+ */
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.TimedRobot;
+
+/**
+ * Assignment 02/01: Finish implementing the Climber subsystem
+ * 
+ * Yesterday (01/31), Sylvia and I created the Climber subsystem. Recall 
+ * that for actuator subsystems, we need the following things:
+ * 
+ * 		- Variables for the commands that we want to issue eventually
+ * 		- Actuator objects to command
+ * 		- Constructor to create the objects needed
+ * 		- Some means of resetting the command variables (reset())
+ * 		- Some means of updating the command variables
+ * 		- Some means of updating the actuators to do the commands (update())
+ * 
+ * Today, we're going to hook the Climber we made up to the rest of our code.
+ * As usual, follow the TODOs. There are xx of them. Good luck! Don't forget
+ * to push the code back up to github when you're done. A reminder on the
+ * commands:
+ * 
+ *  	git add .
+ *  	git commit -am "your message here"
+ *  	git push origin master
+ */
 
 public class Robot extends TimedRobot {
 	// Storage variables
@@ -12,12 +46,22 @@ public class Robot extends TimedRobot {
 	private double m_leftCmd;
 	private double m_rightCmd;
 	
+	/**
+	 * TODO 03: DECLARE NEW OBJECTS
+	 * We need a Compressor object and a Climber object. Declare them
+	 * here. Name them m_compressor and m_climber, respectively.
+	 */
 	// Declare robot subsystems
 	private DriveSystem m_driveSys;
 	private OperatorInterface m_opFace;
 	
 	@Override
 	public void robotInit() {
+		/**
+		 * TODO 04: CONSTRUCT NEW OBJECTS
+		 * Construct m_compressor and m_climber here. Neither constructor
+		 * takes arguments - i.e. there should be nothing in the parentheses
+		 */
 		// Construct robot subsystems
 		this.m_driveSys = new DriveSystem();
 		this.m_opFace = new OperatorInterface();
@@ -27,6 +71,12 @@ public class Robot extends TimedRobot {
 	public void disabledInit() {
 		// Reset all subsystem states
 		this.reset();
+		
+		/**
+		 * TODO 08: DO NOTHING
+		 * We don't want the compressor to do anything when disabled, so leave
+		 * this alone.
+		 */
 	}
 
 	@Override
@@ -42,6 +92,12 @@ public class Robot extends TimedRobot {
 	public void autonomousInit() {
 		// Reset all subsystem states
 		this.reset();
+		
+		/**
+		 * TODO 09: START THE COMPRESSOR
+		 * We want the compressor running in auton. Start it here.
+		 * 		this.m_compressor.start();
+		 */
 	}
 
 	@Override
@@ -57,6 +113,12 @@ public class Robot extends TimedRobot {
 	public void teleopInit() {
 		// Reset all subsystem states
 		this.reset();
+		
+		/**
+		 * TODO 10: START THE COMPRESSOR
+		 * We want the compressor running in teleop. Start it here.
+		 * 		this.m_compressor.start();
+		 */
 	}
 
 	@Override
@@ -64,6 +126,20 @@ public class Robot extends TimedRobot {
 		// Update all sensor subsystems
 		this.m_opFace.update();
 		
+		/**
+		 * TODO 11: GET CLIMBER COMMANDS FROM OPERATOR INTERFACE
+		 * Let's head over to OperatorInterface.java to set it up
+		 * for commands. We'll come back here later.
+		 */
+		/**
+		 * TODO 16: USE CLIMBER COMMAND METHODS
+		 * Now that we've implemented what we need in OperatorInterface.java,
+		 * let's use it. Declare two storage variables up above named
+		 * 		m_fireMainCyl and m_fireExtCyl
+		 * Don't forget to reset them in the reset() method.
+		 * Then, let's get the values here using this.m_opFace.getFireMainCyl()
+		 * and this.m_opface.getFireExtCyl()
+		 */
 		// Get information from sensor subsystems
 		this.m_driveCommand = this.m_opFace.getDriveCmd();
 		this.m_turnCommand = this.m_opFace.getTurnCmd();
@@ -75,21 +151,46 @@ public class Robot extends TimedRobot {
 		this.m_leftCmd = RobotHelper.limit(this.m_leftCmd, -1.0, 1.0);
 		this.m_rightCmd = RobotHelper.limit(this.m_rightCmd, -1.0, 1.0);
 		
+		/**
+		 * TODO 17: COMMAND THE CLIMBER
+		 * Here, we'll use the this.m_climber.fireMainCyl() and
+		 * this.m_climber.fireExtCyl() methods to tell the climber
+		 * what we want it to do when we update it.
+		 */
 		// Set information for actuator subsystems
 		this.m_driveSys.setCommands(this.m_leftCmd, this.m_rightCmd);
 		
+		/**
+		 * TODO 18: UPDATE THE CLIMBER
+		 * Here, we'll update the climber.
+		 * That should be everything needed to get the climber up and running!
+		 * Try it out! Don't forget to push everything to github!
+		 */
 		// Update all actuator subsystems
 		this.m_driveSys.update();
 	}
 	
 	public void reset() {
+		// Reset storage variables
 		this.m_driveCommand = 0.0;
 		this.m_turnCommand = 0.0;
 		this.m_leftCmd = 0.0;
 		this.m_rightCmd = 0.0;
 		
+		/**
+		 * TODO 06: RESET THE CLIMBER
+		 * We want to reset the climber every time we reset the subsystems.
+		 */
+		// Reset subsystems
 		this.m_driveSys.reset();
 		this.m_opFace.reset();
+		
+		/**
+		 * TODO 07: STOP THE COMPRESSOR
+		 * We want to default the compressor to be stopped. We'll manually start it
+		 * when we actually need it. The command is going to be
+		 * 		this.m_compressor.stop();
+		 */
 	}
 	
 	@Override
--- src/org/usfirst/frc/team2789/robot/subsystems/OperatorInterface.java
+++ src/org/usfirst/frc/team2789/robot/subsystems/OperatorInterface.java
@@ -6,6 +6,13 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class OperatorInterface extends Subsystem {
+	/**
+	 * TODO 12: CREATE CLIMBER STORAGE VARIABLES
+	 * We now need climber storage variables that we read from the
+	 * controller. Since we'll be using buttons, the variables
+	 * will be booleans. Let's name them as follows:
+	 * 		m_fireMainCyl, m_fireExtCyl
+	 */
 	// Storage variables
 	private double m_driveCmd;
 	private double m_turnCmd;
@@ -23,6 +30,10 @@ public class OperatorInterface extends Subsystem {
 		this.reset();
 	}
 	
+	/**
+	 * TODO 13: RESET CLIMBER STORAGE VARIABLES
+	 * Set m_fireMainCyl and m_fireExtCyl to a default value of false
+	 */
 	public void reset() {
 		this.m_driveCmd = 0.0;
 		this.m_turnCmd = 0.0;
@@ -32,6 +43,43 @@ public class OperatorInterface extends Subsystem {
 		// Retrieve axis values from joysticks
 		this.m_driveCmd = this.m_pilotStick.getRawAxis(RobotMap.DRIVE_AXIS);
 		this.m_turnCmd = this.m_pilotStick.getRawAxis(RobotMap.TURN_AXIS);
+		
+		/**
+		 * TODO 14: RETRIEVE CLIMBER VALUES FROM JOYSTICKS
+		 * Create button mappings in RobotMap.java:
+		 * 		BTN_MAIN_CYL = 3
+		 * 		BTN_EXT_CYL_FWD = 1
+		 * 		BTN_EXT_CYL_REV = 2
+		 * 
+		 * The buttons on the gamepad are numbered as such:
+		 * 
+		 * 		  3
+		 * 		2   1
+		 * 		  0
+		 * 
+		 * Read the buttons using this.m_pilotStick.getRawButton(RobotMap....).
+		 * This function returns a boolean (true/false) for the button you request.
+		 * Use this information and the following logic to update the climber commands
+		 * 
+		 * 		this.m_fireMainCyl = this.m_pilotStick.getRawButton(RobotMap.BTN_MAIN_CYL);
+		 * 
+		 * 		// Why? we only want to fire the main cylinder when we press and hold
+		 * 		// the corresponding button - i.e. the corresponding button is true for
+		 * 		// a while
+		 * 
+		 * 		if the BTN_EXT_CYL_REV button is pressed:
+		 * 			set this.m_fireExtCyl to false
+		 * 		else if the BTN_EXT_CYL_FWD button is pressed:
+		 * 			set this.m_fireExtCyl to true
+		 * 
+		 * 		// Why? Think about the four situations we can have:
+		 * 		//	1. Neither button is pressed: nothing happens; the cylinder stays in its
+		 * 		//		current position
+		 * 		//	2. BTN_EXT_CYL_REV is pressed: the cylinder is retracted
+		 * 		// 	3. BTN_EXT_CYL_FWD is pressed: the cylinder is extended
+		 *  	// 	4. Both buttons are pressed: the cylinder is retracted; we want this to 
+		 *  	//		be its default state
+		 */
 	}
 	
 	public double getDriveCmd() {
@@ -40,6 +88,18 @@ public class OperatorInterface extends Subsystem {
 	
 	public double getTurnCmd() {
 		return this.m_turnCmd;
+	}
+	
+	/**
+	 * TODO 15: GETTER METHODS FOR CLIMBER COMMANDS
+	 * Fill in the blanks below:
+	 */
+	public boolean getFireMainCyl() {
+		// your code here
+	}
+	
+	public boolean getFireExtCyl() {
+		// your code here
 	}
 
 	@Override
