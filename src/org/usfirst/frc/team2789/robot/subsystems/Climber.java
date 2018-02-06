9749642c6f5fdd57564ea6e40775bf02dfa11c0e
Eman10pll <Eric.taylor0109@gmail.com>
2018-02-05 19:00:48 -0600

    Climber done

--- src/org/usfirst/frc/team2789/robot/subsystems/Climber.java
+++ src/org/usfirst/frc/team2789/robot/subsystems/Climber.java
@@ -1,24 +1,20 @@
 package org.usfirst.frc.team2789.robot.subsystems;
 
+import java.util.Set;
+
 import org.usfirst.frc.team2789.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Climber extends Subsystem {
 	
 	// Storage variables for piston commands
 	private DoubleSolenoid.Value m_mainCylCmd;
-	private DoubleSolenoid.Value m_extCylCmd; 
-	
-	//storage variable for climber motor
-	// TODO: m_climbon should not be a double
-	// Think about what it should be and fix it
-	public static boolean m_climbOn;
-	
-	// TODO: look at the DriveSystem.java class
-	// to see how to set up a Talon
-		this.m_climbMtr = new Talon(RobotMap.MTR_CLIMB);
+	private DoubleSolenoid.Value m_extCylCmd;
+	private Talon m_climbMtr;
+	private boolean m_climbOn;
 	
 	// DoubleSolenoids to control cylinders
 	private DoubleSolenoid m_mainCyl;
@@ -32,58 +28,56 @@ public class Climber extends Subsystem {
 		this.m_extCyl = new DoubleSolenoid(
 				RobotMap.DS_EXT_CYL_FWD,
 				RobotMap.DS_EXT_CYL_REV);
+		this.m_climbMtr = new Talon(RobotMap.MTR_CLIMB);
 		
 		// Reset all values
 		this.reset();
 	}
 	
 	public void reset() {
-		// Reset piston commands to retract
+		// Commands to retract
 		this.m_mainCylCmd = DoubleSolenoid.Value.kReverse;
-		this.m_extCylCmd = DoubleSolenoid.Value.kReverse;	
+		this.m_extCylCmd = DoubleSolenoid.Value.kReverse;
+		this.m_climbOn = false;
 	}
 	
+	//update cylinders
 	public void update() {
-		// Update actuators with commanded values
 		this.m_mainCyl.set(this.m_mainCylCmd);
 		this.m_extCyl.set(this.m_extCylCmd);
 		
-		// TODO: Look at DriveSystem.java to see how to 
 		// command Talons.
-		// To help you with the way the code should be structured,
-		// try this:
-		//
-		// IF m_climbOn is true THEN
-		//      set m_climbMtr to RobotMap.CLIMB_VAL
-		// ELSE
-		//		set m_climbMtr to 0.0
-		
-		
-		
-		if (this.m_climbOn = true; 
-		
-		else if (this.m_climbMtr(RobotMap.CLIMB_VAL)); {
-			= false
-			return
-					this.m_climbMtr = 0.0;
-		
+		if(this.m_climbOn) {
+			this.m_climbMtr.set(RobotMap.CLIMB_VAL);
+		}
+		else {
+			this.m_climbMtr.set(0.0);
+		}
 	}
 		
 	// setter method
-	// TODO: fill this in
+	//TODO
 	public void setClimbOn(boolean climbOn) {
-	}
-
-	public void fireMainCyl(boolean fire) {
-		this.m_mainCylCmd = fire ? DoubleSolenoid.Value.kForward : DoubleSolenoid.Value.kReverse;
-		
-		
+		this.m_climbOn = climbOn;
 	}
 	
+	//Main
+	public void fireMainCyl(boolean fire) {
+		if(fire) {
+			this.m_mainCylCmd = DoubleSolenoid.Value.kForward;
+		}
+		else {
+			this.m_mainCylCmd = DoubleSolenoid.Value.kReverse;
+		}
+	}
+	//Ext
 	public void fireExtCyl(boolean fire) {
-		this.m_extCylCmd = fire ? DoubleSolenoid.Value.kForward : DoubleSolenoid.Value.kReverse;
-		
-		
+		if(fire) {
+			this.m_extCylCmd = DoubleSolenoid.Value.kForward;
+		}
+		else {
+			this.m_extCylCmd = DoubleSolenoid.Value.kReverse;
+		}
 	}
 
 	@Override
